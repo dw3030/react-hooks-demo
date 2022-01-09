08d994f5ba3adcbc33c5309765744490e7874d2f
Dustin Welch <dustinwwelch1@gmail.com>
2022-01-08 20:42:04 -0600

    changed fxn to a class, has same functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import { useState } from "react";
+import { Component } from "react";
 
 // hooks can only be used inside React fxns, not class components
 // hooks must always be called at the top level of the component
 
-function App() {
-  const [color, setColor] = useState("red");
-
-  const [msg, setMsg] = useState("");
-
-  function handleClick() {
-    // setColor("blue");
-    color === "red" ? setColor("blue") : setColor("red");
-    setMsg("Super Secret Message");
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "red",
+      msg: "",
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  return (
-    <>
-      <h1 style={{ background: color }}>React Hooks Demo</h1>
-      <h1>{msg}</h1>
-      <button onClick={handleClick}>Click Me</button>
-    </>
-  );
+  handleClick() {
+    // setColor("blue");
+    this.state.color === "red"
+      ? this.setState({ color: "blue" })
+      : this.setState({ color: "red" });
+
+    this.setState({ msg: "Super Secret Message" });
+  }
+
+  render() {
+    return (
+      <>
+        <h1 style={{ background: this.state.color }}>React Hooks Demo</h1>
+        <h1>{this.state.msg}</h1>
+        <button onClick={this.handleClick}>Click Me</button>
+      </>
+    );
+  }
 }
 
 export default App;
